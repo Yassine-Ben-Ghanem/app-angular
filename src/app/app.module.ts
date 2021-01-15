75ca7a82ea5213c89a07feb1f79d57fbeaf92931
Yassine-Ben-Ghanem <benghanem1999@gmail.com>
2021-01-15 10:10:30 +0100

    setup environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,56 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { ManageCategoriesComponent } from './admin/components/manage-categories/manage-categories.component';
+import { ManageOrdersComponent } from './admin/components/manage-orders/manage-orders.component';
+import { ManageUsersComponent } from './admin/components/manage-users/manage-users.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CategoryDetailsComponent } from './components/category-details/category-details.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { OrderComponent } from './components/order/order.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductFilterPipe } from './pipes/product-filter.pipe';
+import { AlertComponent } from './shared/alert/alert.component';
+import { ApplicationErrorComponent } from './shared/application-error/application-error.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { ResourceNotFoundComponent } from './shared/resource-not-found/resource-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ManageCategoriesComponent,
+    ManageOrdersComponent,
+    ManageUsersComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    CategoryListComponent,
+    CategoryDetailsComponent,
+    ContactComponent,
+    OrderComponent,
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    ProductFilterPipe,
+    AlertComponent,
+    ApplicationErrorComponent,
+    PageNotFoundComponent,
+    ResourceNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
